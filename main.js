ac5763142a287221041b51939638b4e89aa24fd2
sanjar6767 <rasulberdievsanjar@gmail.com>
2025-11-30 17:13:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+let ekran = document.getElementById("ekran")
+let one = document.getElementById("1")
+let two = document.getElementById("2")
+let thre = document.getElementById("3")
+let frio = document.getElementById("4")
+let fife = document.getElementById("5")
+let six = document.getElementById("6")
+let seven = document.getElementById("7")
+let eyt = document.getElementById("8")
+let nine = document.getElementById("9")
+let kop = document.getElementById("kop")
+let bol = document.getElementById("bol")
+let qosh = document.getElementById("qosh")
+let ayir = document.getElementById("ayir")
+let teng = document.getElementById("teng")
+
+one.addEventListener("click", ()=>{
+    ekran.value = "1"
+})
+two.addEventListener("click", ()=>{
+    ekran.value = "2"
+})
+thre.addEventListener("click", ()=>{
+    ekran.value = "3"
+})
+frio.addEventListener("click", ()=>{
+    ekran.value = "4"
+})
+fife.addEventListener("click", ()=>{
+    ekran.value = "5"
+})
+six.addEventListener("click", ()=>{
+    ekran.value = "6"
+})
+seven.addEventListener("click", ()=>{
+    ekran.value = "7"
+})
+eyt.addEventListener("click", ()=>{
+    ekran.value = "8"
+})
+nine.addEventListener("click", ()=>{
+    ekran.value = "9"
+})
+
+kop.addEventListener("click", ()=>{
+    ekran.value = "*"
+})
+bol.addEventListener("click",()=>{
+    ekran.value = "/"
+})
+qosh.addEventListener("click",()=>{
+    ekran.value = "+"
+})
+ayir.addEventListener("click",()=>{
+    ekran.value = "-"
+})
+
+teng.addEventListener("click",()=>{
+    ekran.value = "Bro, u dk"
+})
